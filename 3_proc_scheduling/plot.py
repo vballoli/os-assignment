594b403276a410776d8e7b0944923b4f810287f1
vballoli <balloli.vb@gmail.com>
2020-04-28 16:29:18 +0530

    Fixed graph

--- 3_proc_scheduling/plot.py
+++ 3_proc_scheduling/plot.py
@@ -4,8 +4,13 @@ with open('stats.txt', 'r') as f:
     data = f.readlines()
     waiting_times = data[0].strip('\n').split(',')
     turnaround_times = data[1].strip('\n').split(',')
+    print(waiting_times)
     labels = ['FCFS', 'Priority', 'RoundRobin', 'SJF(NP)', 'SJF(P)']
-    plt.plot(labels, waiting_times, 'ro', label='Avg Waiting times')
-    plt.plot(labels, turnaround_times, 'bo', label='Avg Turnaround times')
-    plt.legend()
+    x_axis = [0, 1, 2, 3, 4]
+    x, y = zip(*sorted(zip(waiting_times, x_axis)))
+    plt.plot(y, x, 'ro', label='Avg Waiting times')
+    x, y = zip(*sorted(zip(turnaround_times, x_axis)))
+    plt.plot(y, x, 'bo', label='Avg turnaround times')
+    plt.xticks(x_axis, labels)
+    plt.legend
     plt.savefig('stats.png')
